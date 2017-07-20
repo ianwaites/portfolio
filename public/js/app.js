893c9996af48cbc6091fb531b2717918934e69d5
Ian Waites <ian.waites@gmail.com>
2017-07-20 11:15:56 -0700

    things are kinda messed up right now

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // my js - new comment
 'use strict';
 
-const projectsView = {};
+var allProjects = [];
 
 function Projects(portDataObj) {
   this.name = portDataObj.name;
@@ -13,45 +13,58 @@ function Projects(portDataObj) {
   this.img = portDataObj.img;
 }
 
-Projects.all = [];
-
 Projects.prototype.toHtml = function() {
   var template = $('#article-template').html();
   var comp = Handlebars.compile(template);
-  // var $newProjects = $('article.template').clone();
-
-  // This cloned article still has a class of template.
+  // var $newArticle = $('article.template').clone();
+  // /* TODO: This cloned article still has a class of template.
   // However, in our modules.css stylesheet, we gave all elements
   // with a class of template a display of none. Let's make
   // sure we're not accidentally hiding our cloned article! */
-  // $($newProjects).removeClass().addClass(this.name);
+  // $($newArticle).removeClass().addClass(this.name);
   //
   //
-  // if (!this.date) $newProjects.addClass('draft');
-  // $newProjects.data('category', this.language);
+  // if (!this.date) $newArticle.addClass('draft');
+  // $newArticle.data('category', this.language);
+
+  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
+  of the current template clone with properties from this particular Article instance.
+  We need to fill in:
+    1. author name,
+    2. author url,
+    3. article title,
+    4. article body, and
+    5. publication date. */
+
+  // Display the date as a relative number of 'days ago'
+  // $newArticle.find('h1').html(this.name);
+  // $newArticle.find('#description').html(this.desc);
+  // $newArticle.find('#course').html(this.course);
+  // $newArticle.find('#date').html(this.date);
+  // $newArticle.find('#lang').html(this.language);
+  // $newArticle.find('#link').html(this.link);
+  // $newArticle.find('#img').html(this.img);
 
 
   this.daysAgo = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
   this.publishStatus = this.date ? `published ${this.daysAgo} days ago` : '(draft)';
-  // $newProjects.append('<hr>');
+  // $newArticle.append('<hr>');
   return comp(this);
 };
 
-Projects.all.sort(function(a, b) {
+portData.sort(function(a, b) {
   // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
   return (new Date(b.date)) - (new Date(a.date));
 });
 
-Projects.all.forEach(function(articleObject) {
+portData.forEach(function(articleObject) {
   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
-  Projects.all.push(new Projects(articleObject));
+  allProjects.push(new Projects(articleObject));
 });
 
-// projectsView.initIndexPage() {
-// Projects.all.forEach(function(article) {
-//   $('#articles').append(article.toHtml());
-// });
-// };
+allProjects.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+});
 
 // jQuery for nav
 $(document).ready(function() {
@@ -66,55 +79,3 @@ $(document).ready(function() {
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 
 });
-
-// Initializing the index page and rendering to the Handlebars template
-projectsView.initIndexPage = function() {
-  Projects.all.forEach(function(article) {
-    $('#articles').append(article.toHtml())
-  });
-};
-
-Projects.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.date)) - (new Date(a.date));
-  });
-
-  rawData.forEach(function(ele) {
-    Projects.all.push(new Projects(ele));
-  })
-}
-
-const successCallback = function(data){
-  console.log(data)
-  localStorage.setItem('rawData', JSON.stringify(data))
-
-  Projects.loadAll(data)
-  projectsView.initIndexPage();
-}
-
-const errorCallback = function(err){
-  console.log(err)
-}
-
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
-Projects.fetchAll = function() {
-  if (localStorage.rawData) {
-    // When rawData is already in localStorage,
-    // we can load it with the .loadAll function above,
-    // and then render the index page (using the proper method on the projectsView object).
-    Projects.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
-    // render the index page
-    projectsView.initIndexPage();
-  } else {
-    // When we don't already have the rawData,
-    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-    // cache it in localStorage so we can skip the server call next time,
-    // then load all the data into Projects.all with the .loadAll function above,
-    // and then render the index page.
-    $.getJSON('data/portData.json')
-      .then(successCallback, errorCallback)
-
-
-  }
-};
